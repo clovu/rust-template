17576e6aab776b342af987e674ae395142d27c74
Clover You <hi@clovu.me>
2025-08-14 05:07:09 +0800

    feat: add error handling and file listing functionality to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,24 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+mod error;
+
+use error::Result;
+
+fn main() -> Result<()> {
     // load env profile
     dotenv::dotenv()?;
 
-    println!("Hello, world!");
+    let files = list_file(".")?;
+
+    println!("Hello, world! {:?}", files);
 
     Ok(())
+}
+
+fn list_file(path: &str) -> Result<Vec<String>> {
+    let files = std::fs::read_dir(path)
+        .map_err(|_| "error file reading dir...")?
+        .filter_map(|re| re.ok())
+        .filter(|e| e.file_type().map(|ft| ft.is_file()).unwrap_or(false))
+        .filter_map(|e| e.file_name().into_string().ok())
+        .collect();
+    Ok(files)
 }
